20dedce3055a3e83efba340b619b6434bec8d0cc
IR~SAGE <sibo.egide98@gmail.com>
2021-04-09 12:46:12 +0200

    Retriving All Task

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
+from .models import Task
 
 @api_view(['GET'])
 def apiOverview(request):
@@ -18,5 +19,10 @@ def apiOverview(request):
     return Response(api_urls)
 
 
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
 
 
